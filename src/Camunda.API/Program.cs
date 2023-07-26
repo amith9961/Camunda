f76968401b844b9ea01ee9260cbc74c06665ed93
amith.v <amith.v@PIC4398>
2023-07-26 19:34:41 +0530

    code changes

--- src/Camunda.API/Program.cs
+++ src/Camunda.API/Program.cs
@@ -1,9 +1,10 @@
+using Camunda.API.Worker;
 using Camunda.Domain.Service;
-using Data.GraphQL;
 using Data.GraphQL.Repository;
 using GraphQL.Client.Http;
 using GraphQL.Client.Serializer.Newtonsoft;
 using MediatR;
+using Newtonsoft.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,6 +25,11 @@ builder.Services.AddSingleton <GraphQLHttpClient>(options =>
     var client = new GraphQLHttpClient(option, new NewtonsoftJsonSerializer());
     return client;
 });
+builder.Services.AddHttpClient();
+
+// Register the ExternalTaskHandlerService as a hosted service
+builder.Services.AddSingleton<ExternalTaskHandlerService>();
+builder.Services.AddHostedService<ExternalTaskHandlerHostedService>();
 builder.Services.AddSingleton<GraphQLRepository>();
 var app = builder.Build();
 
@@ -39,5 +45,61 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
 app.Run();
+
+public class ExternalTaskHandlerHostedService : BackgroundService
+{
+    private readonly ExternalTaskHandlerService _externalTaskHandlerService;
+    private readonly CancellationTokenSource _cancellationTokenSource;
+    private readonly HttpClient _httpClient;
+    private readonly string _camundaApiUrl;
+
+    public ExternalTaskHandlerHostedService(ExternalTaskHandlerService externalTaskHandlerService, IHttpClientFactory httpClientFactory)
+    {
+        _externalTaskHandlerService = externalTaskHandlerService;
+        _cancellationTokenSource = new CancellationTokenSource();
+        _httpClient = httpClientFactory.CreateClient();
+        _camundaApiUrl = "http://localhost:8080/engine-rest/"; // Replace with your Camunda API URL
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        // Poll Camunda every 5 seconds to check if the timer event has occurred
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            var timerEventOccurred = await CheckTimerEventStatusAsync();
+
+            if (timerEventOccurred)
+            {
+                // Call the ExternalTaskHandlerService to start handling external tasks
+                await _externalTaskHandlerService.StartHandlingExternalTasksAsync(_cancellationTokenSource.Token);
+                break; // Exit the loop since the timer event occurred
+            }
+
+            // Wait for 5 seconds before checking again
+            await Task.Delay(TimeSpan.FromSeconds(5), stoppingToken);
+        }
+    }
+
+    public override async Task StopAsync(CancellationToken cancellationToken)
+    {
+        // Stop the ExternalTaskHandlerService when this hosted service is stopped
+        _cancellationTokenSource.Cancel();
+        await base.StopAsync(cancellationToken);
+    }
+
+    private async Task<bool> CheckTimerEventStatusAsync()
+    {
+        var response = await _httpClient.GetAsync($"{_camundaApiUrl}history/activity-instance?activityId=Event_0ajqwj4&finished=true");
+        response.EnsureSuccessStatusCode();
+        var content = await response.Content.ReadAsStringAsync();
+        var activities = JsonConvert.DeserializeObject<List<ActivityInstance>>(content);
+        return activities.Any();
+    }
+}
+
+public class ActivityInstance
+{
+    public string Id { get; set; }
+    public string ActivityId { get; set; }
+}
---
+++ src/Camunda.API/Worker/ExternalTaskHandlerService.cs
@@ -0,0 +1,93 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Threading;
+
+namespace Camunda.API.Worker
+{
+    public class ExternalTaskData
+    {
+        public string Id { get; set; }
+        public string TopicName { get; set; }
+        public JObject Variables { get; set; }
+    }
+    public class ExternalTaskHandlerService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _camundaApiUrl; // Replace with your Camunda API URL
+        private bool _isRunning = false;
+
+        public ExternalTaskHandlerService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            _camundaApiUrl = "http://localhost:8080/engine-rest/"; // Replace with your Camunda API URL
+        }
+        public async Task StartHandlingExternalTasksAsync(CancellationToken cancellationToken)
+        {
+            if (_isRunning)
+            {
+                // Already started, do nothing.
+                return;
+            }
+
+            _isRunning = true;
+
+            try
+            {
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    // Fetch external tasks from Camunda
+                    var externalTasks = await FetchExternalTasksAsync();
+                    foreach (var task in externalTasks)
+                    {
+                        // Handle the task (in this case, we'll update the salary)
+                        await HandleExternalTaskAsync(task);
+                    }
+                }
+            }
+            finally
+            {
+                _isRunning = false;
+            }
+        }
+
+        public async Task FetchAndHandleExternalTasksAsync(CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                // Fetch external tasks from Camunda
+                var externalTasks = await FetchExternalTasksAsync();
+                foreach (var task in externalTasks)
+                {
+                    // Handle the task (in this case, we'll update the salary)
+                    await HandleExternalTaskAsync(task);
+                }
+
+                // Wait for a specific interval before fetching tasks again
+               // await Task.Delay(TimeSpan.FromSeconds(10), cancellationToken);
+            }
+        }
+
+        private async Task<List<ExternalTaskData>> FetchExternalTasksAsync()
+        {
+            var response = await _httpClient.GetAsync($"{_camundaApiUrl}external-task?topicName=updateSalary");
+            response.EnsureSuccessStatusCode();
+            var content = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<List<ExternalTaskData>>(content);
+        }
+        private async Task HandleExternalTaskAsync(ExternalTaskData task)
+        {
+            // Call the .NET Core API endpoint to update the salary
+            var response = await _httpClient.PatchAsync("https://localhost:7094/api/v1/incrementSalary", null);
+            response.EnsureSuccessStatusCode();
+
+            // Complete the external task in Camunda after processing
+            await CompleteExternalTaskAsync(task.Id);
+        }
+
+        private async Task CompleteExternalTaskAsync(string taskId)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Post, $"{_camundaApiUrl}external-task/{taskId}/complete");
+            await _httpClient.SendAsync(request);
+        }
+    }
+}
